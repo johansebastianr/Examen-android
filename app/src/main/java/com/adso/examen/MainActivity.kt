091fc1af9c77add6ccad02d0d9477cba36205679
JohanSebastian <johsrodriguez11111@soy.sena.edu.co>
2024-05-23 14:13:59 -0500

    corregir

--- app/src/main/java/com/adso/examen/MainActivity.kt
+++ app/src/main/java/com/adso/examen/MainActivity.kt
@@ -1,49 +1,20 @@
 package com.adso.examen
 
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.adso.examen.databinding.ActivityMainBinding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnKm.setOnClickListener { convertToKm() }
-        binding.btnCm.setOnClickListener { convertToCm() }
-        binding.btnMm.setOnClickListener { convertToMm() }
-    }
-
-    private fun convertToKm() {
-        val metros = binding.etMetros.text.toString().toDoubleOrNull() ?: return
-        val km = metros / 1000
-        mostrarAlerta("$metros metros son $km km")
-    }
-
-    private fun convertToCm() {
-        val metros = binding.etMetros.text.toString().toDoubleOrNull() ?: return
-        val cm = metros * 100
-        mostrarAlerta("$metros metros son $cm cm")
-    }
-
-    private fun convertToMm() {
-        val metros = binding.etMetros.text.toString().toDoubleOrNull() ?: return
-        val mm = metros * 1000
-        mostrarAlerta("$metros metros son $mm mm")
-    }
-
-    private fun mostrarAlerta(mensaje: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setMessage(mensaje)
-            .setPositiveButton("Aceptar") { dialog, _ ->
-                dialog.dismiss()
-            }
-        val dialog = builder.create()
-        dialog.show()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
-
