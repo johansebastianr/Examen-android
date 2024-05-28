bddbb05c0ad3b79579b5016dc9b2e522de5d19f2
JohanSebastian <johsrodriguez11111@soy.sena.edu.co>
2024-05-28 16:53:32 -0500

    examen corregido

--- app/src/main/java/com/adso/examen/MainActivity.kt
+++ app/src/main/java/com/adso/examen/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.adso.examen
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.adso.examen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.kilometros.setOnClickListener { KmConversion() }
+        binding.centimetros.setOnClickListener { CmConversion() }
+        binding.milimetros.setOnClickListener { MmConversion() }
+        binding.decametros.setOnClickListener { DamConversion() }
+    }
+
+    private fun KmConversion() {
+        val metrosIngresados = binding.metrosIngresados.text.toString().toDoubleOrNull() ?: return
+        val km = metrosIngresados / 1000
+        mostrarResultado("$metrosIngresados metros son $km km")
+    }
+
+    private fun CmConversion() {
+        val metrosIngresados = binding.metrosIngresados.text.toString().toDoubleOrNull() ?: return
+        val cm = metrosIngresados * 100
+        mostrarResultado("$metrosIngresados metros son $cm cm")
+    }
+
+    private fun MmConversion() {
+        val metrosIngresados = binding.metrosIngresados.text.toString().toDoubleOrNull() ?: return
+        val mm = metrosIngresados * 1000
+        mostrarResultado("$metrosIngresados metros son $mm mm")
+    }
+
+    private fun DamConversion() {
+        val metrosIngresados = binding.metrosIngresados.text.toString().toDoubleOrNull() ?: return
+        val dam = metrosIngresados * 0.1
+        mostrarResultado("$metrosIngresados metros son $dam decametros")
+    }
+
+    private fun mostrarResultado(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
